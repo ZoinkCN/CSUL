f31eeeca321ebdcca1afdd098e3bf46b0623a62c
ZoinkCN <zoinkcn@outlook.com>
2023-12-10 22:38:49 +0800

    增加当前安装的游戏版本显示

--- CSUL/Models/GameManager.cs
+++ CSUL/Models/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 
 namespace CSUL.Models
 {
@@ -18,6 +19,11 @@ namespace CSUL.Models
         /// 得到<see cref="GameManager"/>的实例
         /// </summary>
         public static GameManager Instance { get; } = new();
+
+        /// <summary>
+        /// 已安装的游戏版本
+        /// </summary>
+        public static string? GameVersion => GetGameVersion();
 
         #region ---构造函数---
 
@@ -72,6 +78,7 @@ namespace CSUL.Models
         /// </summary>
         [Config]
         public string? SteamPath { get; set; } = null;
+
         #endregion ---公共属性---
 
         #region ---静态方法---
@@ -84,10 +91,37 @@ namespace CSUL.Models
         {
             if (string.IsNullOrEmpty(gamePath)) throw new ArgumentNullException(nameof(gamePath));
             if (!File.Exists(gamePath)) throw new FileNotFoundException($"游戏路径{gamePath}不存在，请检查路径设置", gamePath);
-            if (arguments is null)  Process.Start(gamePath);
+            if (arguments is null) Process.Start(gamePath);
             else Process.Start(gamePath, arguments);
         }
 
+        /// <summary>
+        /// 获取当前安装的游戏版本
+        /// </summary>
+        /// <returns></returns>
+        public static string? GetGameVersion()
+        {
+            string gameFilePath = Path.Combine(FileManager.Instance.GameRootDir.FullName, @"Cities2_Data\Managed\Game.dll");
+            Assembly gameAssembly = Assembly.LoadFrom(gameFilePath);
+            Type? gameVersion = gameAssembly.GetType("Game.Version");
+            FieldInfo? fieldInfo1 = gameVersion?.GetField("current");
+            object? versionObject = fieldInfo1?.GetValue(null);
+
+            if (versionObject != null)
+            {
+                string colossalFilePath = Path.Combine(FileManager.Instance.GameRootDir.FullName, @"Cities2_Data\Managed\Colossal.Core.dll");
+                Assembly colossaAssembly = Assembly.LoadFrom(colossalFilePath);
+                Type? colossalVersion = colossaAssembly.GetType("Colossal.Version");
+                PropertyInfo? shortVersionFieldInfo = colossalVersion?.GetProperty("shortVersion");
+                PropertyInfo? versionFieldInfo = colossalVersion?.GetProperty("version");
+                PropertyInfo? fullVersionFieldInfo = colossalVersion?.GetProperty("fullVersion");
+                string? shortVersion = (string?)shortVersionFieldInfo?.GetValue(versionObject);
+                string? version = (string?)versionFieldInfo?.GetValue(versionObject);
+                string? fullVersion = (string?)fullVersionFieldInfo?.GetValue(versionObject);
+                return shortVersion;
+            }
+            return null;
+        }
         #endregion ---静态方法---
     }
 }
